b2bbdf5a98764bf4fb09fdd1f53421e91804aaa6
Cyril Balit <cbalit@gmail.com>
2015-04-03 19:37:27 +0200

    step-4

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,3 @@
 "use strict";
 
 var angularMovieApp = angular.module('angularMovieApp', ['ngRoute']);
-
-angularMovieApp.config(function($routeProvider) {
-    $routeProvider
-        .when('/home', {
-            templateUrl: 'partials/home.html',
-            controller : 'homeController'
-        })
-        .when('/movies', {
-            templateUrl: 'partials/movies.html',
-            controller : 'moviesController'
-        })
-        .otherwise({
-            redirectTo: '/home'
-        });
-});
